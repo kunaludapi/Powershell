3231da9b850b84f3044b41c3992772672250a097
Kunal Udapi <kunaludapi@gmail.com>
2019-02-14 16:44:18 +0530

    realtime gui form

---
+++ Tools/realtime-powershell-gui.ps1
@@ -0,0 +1,18 @@
+#real time data Auto update form
+
+function UpdateProcCount($Label)
+{
+    $Label.Text = "Number of processes running on my computer: " + (Get-Process | measure).Count
+}
+
+$Form = New-Object System.Windows.Forms.Form
+$timer = New-Object System.Windows.Forms.Timer
+$timer.Interval = 1000
+$timer.Add_Tick({UpdateProcCount $Label})
+$timer.Enabled = $True
+$Form.Text = "Sample Form"
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "Number of process executed on my computer"
+$Label.AutoSize = $True
+$Form.Controls.Add($Label)
+$Form.ShowDialog()
