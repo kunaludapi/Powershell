2a6f805b3f6f99c92f8c482bbdc5534c52ec994b
Kunal Udapi <kunaludapi@gmail.com>
2019-04-02 09:53:04 +0530

    Delete POWERSHELL CONNECT TO ALL OFFICE 365 SERVICES WITH SUPPORT FOR MFA.ps1

--- Tools/POWERSHELL CONNECT TO ALL OFFICE 365 SERVICES WITH SUPPORT FOR MFA.ps1
+++
@@ -1,7 +0,0 @@
-function Connect-O365 
-{
-	$UserCredential = Get-Credential
-	$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell/" -Credential $UserCredential -Authentication Basic -AllowRedirection
-	Import-PSSession $Session
-	Connect-MsolService -Credential $UserCredential
-}
