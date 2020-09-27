dc54dcbff69db8e19fa6d72000a1a4f1f68a90fd
Kunal Udapi <kunaludapi@gmail.com>
2020-09-27 19:36:06 +0530

    Powershell slice array into smaller group of array
    Powershell slice array into smaller group of array

---
+++ Get-SlicedArray.ps1
@@ -0,0 +1,33 @@
+﻿$numbers = 1..100
+$groupOf = 20
+
+for ($i = 0; $i -le 100; $i+= $groupOf)
+{
+    if ($numbers.count -eq $i)
+    {
+        break
+    }        
+    $group = $i + $groupOf
+    $numbers[$i..$group] -join ', ' 
+    $i++
+}
+
+
+
+$numbers = 1..100
+$groupOf = 9
+
+$result = New-Object System.Collections.ArrayList
+
+for ($i = 0; $i -le 100; $i+= $groupOf)
+{
+    if ($numbers.count -eq $i)
+    {
+        break
+    }
+    $group = $i + $groupOf   
+    [void]$result.add($numbers[$i..$group])
+    $i++
+}
+
+$result[3]
